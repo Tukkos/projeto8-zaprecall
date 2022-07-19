21ea2a05b3dca0734d984592004e8303bf63a468
Tukkos <jpfcastro@id.uff.br>
2022-07-19 12:25:47 -0300

    Added: Welcome Window and a function to hide this window

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import WelcomeWindow from "./WelcomeWindow";
+
+export default function App() {
+    return (
+        <div>
+            <WelcomeWindow />
+        </div>
+    );
+}
---
+++ src/components/WelcomeWindow.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+export default function WelcomeWindow() {
+    const [welcomeWindowClass, setWelcomeWindowClass] = useState("welcomeWindow");
+
+    return (
+        <div className={welcomeWindowClass}>
+            <img src="/assets/images/logo.png" alt="" className="welcomeWindowImage" />
+            <h1>ZapRecall</h1>
+            <div className="startButton"
+                onClick={() => {setWelcomeWindowClass("welcomeWindow hidden");
+                console.log("foi");
+                }}
+                ><p>Iniciar Recall!</p></div>
+        </div>
+    );
+}
